884225f2ac3c8cd94c399ee501ff1bd8ac6ff78f
Palidan MAIMAITI <pmaimait@paul-f5Br6s6.clusters.42paris.fr>
2024-03-15 14:48:21 +0100

    15Mars

--- backend/deep_thought/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import AnswerToEverything
-
-urlpatterns = [
-    path('answer/', AnswerToEverything.as_view(), name='answer-to-everything'),
-]
--- backend/pong_game/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r"ws/game/$", consumers.LocalConsumer.as_asgi()),
-    re_path(r"ws/online/$", consumers.OnlineConsumer.as_asgi()),
-]

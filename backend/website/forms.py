efb7e631219a175fabb5ea40c85a8fa110b605a3
Hinda FERJANI <hferjani@paul-f5Br3s5.clusters.42paris.fr>
2024-05-09 17:15:53 +0200

    remove all files

--- backend/website/forms.py
+++
@@ -1,7 +0,0 @@
-from .models import CustomUser
-from django.contrib.auth.forms import UserCreationForm
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = CustomUser
-        fields = ('username', 'email', 'password1')  # You can customize the fields as needed

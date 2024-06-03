dd734cb3f0e5b1812bc91d36e8f1ca354c204180
Hinda FERJANI <hferjani@paul-f5Ar1s5.clusters.42paris.fr>
2024-06-03 15:21:54 +0200

    add jQuery

--- backend/website/static/scripts/login.js
+++ backend/website/static/scripts/login.js
@@ -1,22 +1,24 @@
 console.log('login.js');
 
 
-const loadjQuery2 = () => {
-    return new Promise((resolve, reject) => {
-        if (typeof window.jQuery !== 'undefined') {
-			console.log('jQuery already loaded in login.js');
-            resolve(); // jQuery already loaded
-        } else {
-            const script = document.createElement('script');
-            script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
-            script.onload = resolve;
-            script.onerror = reject;
-            document.head.appendChild(script);
-        }
-    });
-};
+// const loadjQuery = () => {
+//     return new Promise((resolve, reject) => {
+//         if (typeof window.jQuery !== 'undefined') {
+// 			console.log('jQuery already loaded in login.js');
+//             resolve(); // jQuery already loaded
+//         } else {
+//             const script = document.createElement('script');
+//             script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
+//             script.onload = resolve;
+//             script.onerror = reject;
+//             document.head.appendChild(script);
+//         }
+//     });
+// };
 
-loadjQuery2()
+// import { loadjQuery } from './utils.js';
+
+loadjQuery()
     .then(() => {
         $(document).ready(function(){
             $('#loginForm').submit(function(event){
--- backend/website/static/scripts/register.js
+++ backend/website/static/scripts/register.js
@@ -1,20 +1,22 @@
 console.log('register.js');
 
 
-const loadjQuery = () => {
-    return new Promise((resolve, reject) => {
-        if (typeof window.jQuery !== 'undefined') {
-			console.log('jQuery already loaded in login.js');
-            resolve(); // jQuery already loaded
-        } else {
-            const script = document.createElement('script');
-            script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
-            script.onload = resolve;
-            script.onerror = reject;
-            document.head.appendChild(script);
-        }
-    });
-};
+// const loadjQuery = () => {
+//     return new Promise((resolve, reject) => {
+//         if (typeof window.jQuery !== 'undefined') {
+// 			console.log('jQuery already loaded in login.js');
+//             resolve(); // jQuery already loaded
+//         } else {
+//             const script = document.createElement('script');
+//             script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
+//             script.onload = resolve;
+//             script.onerror = reject;
+//             document.head.appendChild(script);
+//         }
+//     });
+// };
+
+// import { loadjQuery } from './utils.js';
 
 loadjQuery()
     .then(() => {

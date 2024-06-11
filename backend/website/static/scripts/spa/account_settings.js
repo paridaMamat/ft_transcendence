033cf0bf0c9e593cec3fbee93a39196d46587199
Hinda FERJANI <hferjani@paul-f4Br5s5.clusters.42paris.fr>
2024-06-11 11:28:23 +0200

    Integrating changes from origin/new_Hinda and my local modifications

--- backend/website/static/scripts/spa/account_settings.js
+++ backend/website/static/scripts/spa/account_settings.js
@@ -1,23 +1,21 @@
 console.log('accout_settings.js');
 
-import * as Menu from './modules/load.js';
 
-
-	async function getUserInfo() {
-	  const response = await fetch('/api/user-info/'); // Replace with your API endpoint
+	// async function getUserInfo() {
+	//   const response = await fetch('/api/user-info/'); // Replace with your API endpoint
 	
-	  if (response.ok) {
-		const data = await response.json();
-		document.getElementById('username').placeholder = data.username;
-		document.getElementById('first_name').placeholder = data.first_name;
-		document.getElementById('last_name').placeholder = data.last_name;
-		document.getElementById('email').placeholder = data.email; // Include if you want to expose email
-	  } else {
-		console.error('Error fetching user info:', response.statusText);
-	  }
-	}
+	//   if (response.ok) {
+	// 	const data = await response.json();
+	// 	document.getElementById('username').placeholder = data.username;
+	// 	document.getElementById('first_name').placeholder = data.first_name;
+	// 	document.getElementById('last_name').placeholder = data.last_name;
+	// 	document.getElementById('email').placeholder = data.email; // Include if you want to expose email
+	//   } else {
+	// 	console.error('Error fetching user info:', response.statusText);
+	//   }
+	// }
 	
-	// Call the function on window load (or after successful login)
-	window.onload = getUserInfo;
-  Menu.getMenuInfos();
+	// // Call the function on window load (or after successful login)
+	// window.onload = getUserInfo;
+  // getMenuInfos();
 

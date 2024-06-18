170e65bf28573e6fde8caf56199f30c368b5d045
Parida MAMAT <parda1128@hotmail.com>
2024-06-18 12:05:48 +0200

    after merge some modif

---
+++ backend/website/api/auth42_api.py
@@ -0,0 +1,25 @@
+from rest_framework.views import APIView
+from django.http import JsonResponse
+from rest_framework.permissions import AllowAny
+from django.conf import settings
+import logging
+
+logger = logging.getLogger(__name__)
+
+class AuthUrlView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        client_id = settings.CLIENT_ID
+        redirect_uri = settings.REDIRECT_URI
+        scopes = 'public'
+
+        logger.debug(f"redirect_uri ## {redirect_uri}")
+        logger.debug(f"client_id ## {client_id}")
+
+        auth_url = (
+            f'https://api.intra.42.fr/oauth/authorize?'
+            f'client_id={client_id}&redirect_uri={redirect_uri}&'
+            f'scope={scopes}&response_type=code'
+        )
+        return JsonResponse({'authUrl': auth_url})
--- backend/website/urls.py
+++ backend/website/urls.py
@@ -1,53 +1,33 @@
 from django.urls import path, include
-from . import views
-from .views import *
+from .api import *
+from .api.customUser_api import CustomUserViewSet
+from .api.lobby_api import LobbyViewSet, UserInLobbyViewSet
+from .api.party_api import PartyViewSet
+from .api.partyInTour_api import PartyInTournamentViewSet
+from .api.game_api import GameViewSet
+# from .api.auth42_api import AuthUrlViewSet
+from .api.userStats_api import UserStatsViewSet
+from .api.tournament_api import TournamentViewSet
 #from django.conf import settings
 #from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 #from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
-router.register('users', CustomUserViewSet, basename='users')
-router.register('party', PartyViewSet, basename='party')
-router.register('party_in_tour', PartyInTournamentViewSet, basename='party_in_tour')
-router.register('user_lobby', UserInLobbyViewSet, basename='user_lobby')
-router.register('tournament', TournamentViewSet, basename='tournament')
-router.register('game', GameViewSet, basename='game')
+router.register(r'users', CustomUserViewSet, basename='users') # to get current user infos, use /users/me
+router.register(r'party', PartyViewSet, basename='party')
+router.register(r'party_in_tour', PartyInTournamentViewSet, basename='party_in_tour')
+router.register(r'lobbies', LobbyViewSet, basename='lobbies')
+router.register(r'user_in_lobby', UserInLobbyViewSet, basename='user_in_lobby') # to get current user infos, use /user_in_lobby/me
+router.register(r'tournament', TournamentViewSet, basename='tournament')
+router.register(r'game', GameViewSet, basename='game')
+router.register(r'user_stats', UserStatsViewSet, basename='user_stats') # to get current user infos, use /user_stats/me
+# router.register(r'auth42', AuthUrlViewSet, basename='auth42')
 
 urlpatterns = [
 	path('', include(router.urls)),
-	#path('user_list/', views.user_list, name='user_list'),
-	#path('user_detail/<int:id>/', views.user_detail, name='user_detail'),
-	#path('user_info/', views.user_detail, name='user_info'),
-	#path('games_list/', GameViewSet.as_view({'get':'list'}), name='games_list'),
-	##path('games_list/', views.games_list, name='games_list'),
-	#path('game_detail/<int:id>/', views.game_detail, name='game_detail'),
-	#path('game_info/', views.game_info, name='game_info'),
-	
-    #path('parties_list/', PartyViewSet.as_view({'get':'list'}), name='parties_list'),
-	##path('parties_list/', views.parties_list, name='parties_list'),
-	#path('party_detail/<int:id>/', views.party_detail, name='party_detail'),
-	#path('party_info/', views.party_info, name='party_info'),
-	
-    ##path('parties_tour_list/', PartyInTournamentViewSet.as_view({'get':'list'}), name='partiesStats_list'),
-	#path('parties_stats_list/', views.parties_stats_list, name='partiesStats_list'),
-	#path('party_stats_detail/<int:id>/', views.party_stats_detail, name='party_stats_detail'),
-	#path('party_tour_info/', views.party_stats_info, name='party_stats_info'),
-	
-    #path('lobbies_list/', LobbyViewSet.as_view({'get':'list'}), name='lobbys_list'),
-	##path('lobbies_list/', views.lobbies_list, name='lobbies_list'),
-	#path('lobby_detail/<int:id>/', views.lobby_detail, name='lobby_detail'),
-	#path('lobby_info/', views.lobby_info, name='lobby_info'),
-	
-    #path('user_in_lobbies_list/', UserInLobbyViewSet.as_view({'get':'list'}), name='user_in_lobby_list'),
-	##path('user_in_lobbies_list/', views.user_in_lobbies_list, name='user_in_lobbies_list'),
-	#path('user_in_lobby_detail/<int:id>/', views.user_in_lobby_detail, name='user_in_lobby_detail'),
-	#path('user_in_lobby_info/', views.user_in_lobby_info, name='user_in_lobby_info'),
-	
-    #path('tournaments_list/', TournamentViewSet.as_view({'get':'list'}), name='tournaments_list'),
-	##path('tournaments_list/', views.tournaments_list, name='tournaments_list'),
-	#path('tournament_detail/<int:id>/', views.tournament_detail, name='tournament_detail'),
-	#path('tournament_info/', views.tournament_info, name='tournament_info'),
+	# path('update_alias/<int:pk>/', CustomUserViewSet.update_alias, name='update_alias'),
+    # path('retrieve5first/', UserStatsViewSet.retrieve5first, name='retrieve5first'),
 ]
 
 urlpatterns += router.urls

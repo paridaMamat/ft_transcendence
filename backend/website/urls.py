3f4c52aca27bd213081bb2447d5c0e1a44be6a4c
Parida MAMAT <parda1128@hotmail.com>
2024-06-18 15:07:35 +0200

    18/juin api42 is working

--- backend/website/urls.py
+++ backend/website/urls.py
@@ -5,13 +5,10 @@ from .api.lobby_api import LobbyViewSet, UserInLobbyViewSet
 from .api.party_api import PartyViewSet
 from .api.partyInTour_api import PartyInTournamentViewSet
 from .api.game_api import GameViewSet
-# from .api.auth42_api import AuthUrlViewSet
 from .api.userStats_api import UserStatsViewSet
 from .api.tournament_api import TournamentViewSet
-#from django.conf import settings
-#from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
-#from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 router = DefaultRouter()
 router.register(r'users', CustomUserViewSet, basename='users') # to get current user infos, use /users/me
@@ -22,7 +19,6 @@ router.register(r'user_in_lobby', UserInLobbyViewSet, basename='user_in_lobby') 
 router.register(r'tournament', TournamentViewSet, basename='tournament')
 router.register(r'game', GameViewSet, basename='game')
 router.register(r'user_stats', UserStatsViewSet, basename='user_stats') # to get current user infos, use /user_stats/me
-# router.register(r'auth42', AuthUrlViewSet, basename='auth42')
 
 urlpatterns = [
 	path('', include(router.urls)),
